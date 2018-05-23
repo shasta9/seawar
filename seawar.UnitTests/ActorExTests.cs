511f8152dab626fb52b5afe41109993b0b3f058c
John Hutchinson <john.hutchinson@pcs-instruments.com>
2018-05-23 21:31:46 +0100

    WIP - exploring new classes

---
+++ seawar.UnitTests/ActorExTests.cs
@@ -0,0 +1,8 @@
+﻿using NUnit.Framework;
+
+namespace seawar.UnitTests {
+   [TestFixture]
+   public class ActorExTests {
+
+   }
+}
--- seawar/Actors/Actor.cs
+++ seawar/Actors/Actor.cs
@@ -6,7 +6,7 @@ using seawar.Physics;
 using seawar.Vectors;
 
 namespace seawar.Actors {
-   
+
    public class Actor : IMoveable {
 
       private readonly List<IAction> actions = new List<IAction>();
@@ -56,4 +56,85 @@ namespace seawar.Actors {
       public void Collide(MoveResult result, double speed) {
       }
    }
+
+   public class ActorEx {
+      private readonly InBox inBox;
+      private readonly OutBox outBox;
+      private readonly World world;
+      private readonly IPhysicsEx physics;
+
+      public ActorEx(InBox inBox, OutBox outBox, World world, IPhysicsEx physics) {
+         this.inBox = inBox;
+         this.outBox = outBox;
+         this.world = world;
+         this.physics = physics;
+      }
+
+      public string Name { get; set; } = string.Empty;
+      public Vec Position { get; set; } = Vec.Zero;
+      public double BaseSpeed { get; set; } = 1.0;
+      public bool HasExpired { get; set; }
+
+      public void StartMove(Move move) {
+         physics.StartMove(move);
+      }
+
+      public void StopMove() {
+         physics.StopMove();
+      }
+
+      public void Update(Duration delta) {
+         // get messages
+         // update physics
+         physics.Update(world, delta);
+      }
+   }
+
+   public interface IPhysicsEx {
+      void StartMove(Move move);
+      void StopMove();
+      void Update(World world, Duration delta);
+   }
+
+   public class DestroyerPhysics : IPhysicsEx {
+      private readonly ActorEx destroyer;
+      private readonly Mover mover;
+      private Move currentMove;
+
+      public DestroyerPhysics(ActorEx destroyer) {
+         this.destroyer = destroyer;
+         mover = new Mover(this);
+      }
+
+      public void StartMove(Move move) {
+         currentMove = move;
+      }
+
+      public void StopMove() {
+         currentMove = null;
+      }
+
+      public void Update(World world, Duration delta) {
+         if (currentMove != null) {
+            var result = mover.Update(delta);
+
+         }
+      }
+   }
+
+   public class Mover {
+      private IPhysicsEx physics;
+
+      public Mover(IPhysicsEx physics) {
+         this.physics = physics;
+      }
+
+      public MoveResult Update(Duration delta) {
+         throw new System.NotImplementedException();
+      }
+   }
+
+   public class OutBox { }
+
+   public class InBox { }
 }
